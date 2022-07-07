ce2d4e62fc7cff0795cc01657d2dc5ac1468e788
NChang007 <nelsonc924@gmail.com>
2022-07-07 23:12:44 +0000

    finished todos api

--- src/app.py
+++ src/app.py
@@ -5,8 +5,6 @@ todos = [
     { "label": "My second task", "done": False }
 ]
 
-
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/todos', methods=['GET'])
@@ -18,11 +16,18 @@ def hello_world():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.get_json(force=True)
+    if request_body:
+      todos.append(request_body)
     # print("Incoming request with the following body", request_body)
-    if request_body.is_json:
-      todo = request_body.get_json()
-      return todo
-    return 'Response for the POST todo'
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if len(todos) > position - 1:
+      todos.pop(position)
+      return jsonify(todos)
+    
+    return "todo not found"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
